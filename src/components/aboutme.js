92625d51535631024247f2eea87f34cd2624ba37
Mike Carter <elite.mikecarter@gmail.com>
2021-04-06 12:47:29 -0400

    updated about me page

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 class About extends Component {
   render() {
-    return(
-      <div><h1>About Page</h1></div>
-    )
+    return (
+      <div>
+        <h1>Bio</h1>
+
+        <p>
+          {" "}
+          I'm a full stack developer with experience in serverside development,
+          front-end framworks, and various javascript libraries. including React,
+          Node.js, jquery, MySQL, and MongoDB, {" "}
+        </p>
+      </div>
+    );
   }
 }
 
