d6f68427f7cb13ec4af17b4699a646092710a929
Mike Carter <elite.mikecarter@gmail.com>
2021-04-06 22:52:00 -0400

    updated contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -14,7 +14,8 @@ class Contact extends Component {
               alt="pic of Mike Carter"
               style={{height: '250px'}}
                />
-             <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+             <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>U.S Army Veteran</p>
+             <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Full Stack Developer</p>
 
           </Cell>
           <Cell col={6}>
@@ -26,28 +27,21 @@ class Contact extends Component {
                 <ListItem>
                   <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
                     <i className="fa fa-phone-square" aria-hidden="true"/>
-                    (123) 456-7890
+                    (602) 488-0985
                   </ListItemContent>
                 </ListItem>
 
                 <ListItem>
-                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
-                    <i className="fa fa-fax" aria-hidden="true"/>
-                    (123) 456-7890
-                  </ListItemContent>
-                </ListItem>
-
-                <ListItem>
-                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                  <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton'}}>
                     <i className="fa fa-envelope" aria-hidden="true"/>
-                    someone@example.com
+                    elite.mikecarter@gmail.com
                   </ListItemContent>
                 </ListItem>
 
                 <ListItem>
-                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
-                    <i className="fa fa-skype" aria-hidden="true"/>
-                    MySkypeID
+                  <ListItemContent style={{fontSize: '21px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-linkedin-square" aria-hidden="true"/>
+                    https://www.linkedin.com/in/mike-carter-85156ba7/
                   </ListItemContent>
                 </ListItem>
 
