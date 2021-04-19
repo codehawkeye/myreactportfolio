c32d0426154d9860d5efb67c72821fc21d1f5b00
Mike Carter <elite.mikecarter@gmail.com>
2021-04-19 12:57:47 -0400

    updated about me page

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,25 +1,48 @@
 import React, { Component } from "react";
-import { Cell, Grid } from "react-mdl";
+import { Card, CardTitle, CardActions, Grid, Cell } from "react-mdl";
 
 class About extends Component {
   render() {
     return (
-      <div style={{textAlign: 'center'}}>
-        <Grid>
-          <Cell col={4}>
-        <h1>About Me</h1>
-
-        <p>
-          {" "}
-          I'm a full stack developer with a a passion for great useability and design.
-          I have experience with serverside development,
-          front-end framworks, and various javascript libraries: React,
-          Node.js, jquery, MySQL, and MongoDB.  {" "}
-            </p>            
-            </Cell>
+      <Grid className="demo-grid-1">
+      <Cell col={3}></Cell>
+      <Cell col={4}> <div style={{ textAlign: "center" }}>
+        <Card
+          shadow={0}
+          style={{
+            paddingTop: "10",
+            width: "600px",
+            height: "500px",
+            background: "#3E4EB8",
+          }}
+        >
+          <CardTitle expand style={{ alignItems: "flex-start", color: "#fff" }}>
+            <h2 style={{ marginTop: "0" }}></h2>
+            
+            <h4>
+              {" "}
+              I'm a full stack developer with a a passion for great usability
+              and design. I have experience with server-side development,
+              front-end frameworks, and various javascript libraries: React,
+              Node.js, jQuery, MySQL, and MongoDB.{" "}
+            </h4>
+          </CardTitle>
+          <CardActions
+            border
+            style={{
+              borderColor: "rgba(255, 255, 255, 0.2)",
+              display: "flex",
+              boxSizing: "border-box",
+              alignItems: "center",
+              color: "#fff",
+            }}
+          ></CardActions>
+        </Card>
+        </div></Cell>
+      <Cell col={4}>4</Cell>
+ 
+     
         </Grid>
-        
-      </div>
     );
   }
 }
